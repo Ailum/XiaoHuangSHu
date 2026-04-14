b05ad43919fa7b2800eae160b05c2fe23889fb0a
Ailum <dahehedong@gmail.com>
2026-04-14 16:51:40 +0800

    定义角色-权限Redis Key

--- xiaohuangshu-auth/src/main/java/com/lhj/xiaohuangshuauth/constant/RedisKeyConstants.java
+++ xiaohuangshu-auth/src/main/java/com/lhj/xiaohuangshuauth/constant/RedisKeyConstants.java
@@ -23,4 +23,17 @@ public class RedisKeyConstants {
     public static String buildUserRoleKey(String phone){
         return USER_ROLES_KEY_PREFIX + phone;
     }
+  //角色对应的权限集合KEY前缀
+    private static String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
+
+    /**
+     * 构建角色对应的权限集合 KEY
+     * @param roleId
+     * @return
+     */
+
+    public static String buildRolePermissionKey(String roleId){
+        return ROLE_PERMISSIONS_KEY_PREFIX + roleId;
+    }
+
 }

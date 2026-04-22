10e72bfa79a6d66203063c3357792c809893104f
Ailum <dahehedong@gmail.com>
2026-04-22 09:43:06 +0800

    开始加装网关

--- xiaohuangshu-auth/src/main/java/com/lhj/xiaohuangshuauth/controller/TestController.java
+++ xiaohuangshu-auth/src/main/java/com/lhj/xiaohuangshuauth/controller/TestController.java
@@ -1,18 +1,32 @@
 package com.lhj.xiaohuangshuauth.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.lhj.xiaohuangshuauth.alarm.AlarmInterface;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class TestController {
+
+    @Resource
+    private AlarmInterface alarm;
+
     @Value("${rate-limit.api.limit}")
     private Integer limit;
 
     @GetMapping("/test")
-    public String test(){
-        return "当前限流阈值为："+limit;
+    public String test() {
+        return "current rate limit is: " + limit;
+    }
+
+    @GetMapping("/alarm")
+    public String sendAlarm() {
+        alarm.send("系统出错了，你坝马上上线解决");
+        return "alarm success";
     }
 }
